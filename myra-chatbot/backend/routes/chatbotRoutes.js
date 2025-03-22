4e75f8ca88ad7c51b672d6741dc58b1dd91d24c9
robert-calvin-dev <robert.calvin.dev@gmail.com>
2025-03-21 23:23:17 -0700

    Initial commit for The Pour Choice site

--- myra-chatbot/backend/routes/chatbotRoutes.js
+++
@@ -1,9 +0,0 @@
-// myra-chatbot/backend/routes/chatbotRoutes.js
-import express from "express";
-import { getChatResponse } from "../controllers/chatbotController.js";
-
-const router = express.Router();
-
-router.post("/", getChatResponse);
-
-export default router;
